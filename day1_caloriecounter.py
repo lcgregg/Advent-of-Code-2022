5d9f9fd0f3a7d7de39e19644d78f31b276695d6a
Lauren Gregg <lcgregg4@gmail.com>
2022-12-04 15:20:24 -0500

    return top 3 most calories

--- day1_caloriecounter.py
+++ day1_caloriecounter.py
@@ -2,20 +2,22 @@ import heapq
 
 def calorie_counter(inventory):
     '''
-    return max_cals being carried
+    return array of top 3 max_cals being carried
         inventory is a txt file where the elves write the calories of the items
-        a given elf's items are separated by a blank line\
+        a given elf's items are separated by a blank line
     '''
     calorie_count = 0
-    max_cals = 0
+    max_cals = []
 
     f = open(inventory, "r")
     for x in f:
         if x!='\n':
             calorie_count += float(x)
         else:
-            max_cals = max(max_cals, calorie_count)
+            heapq.heappush(max_cals, calorie_count)
             calorie_count = 0
+            if len(max_cals) > 3:
+                heapq.heappop(max_cals)
     f.close()
 
     return max_cals
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from day1_caloriecounter import calorie_counter
 
 if __name__ == "__main__":
     max_cals = calorie_counter("input/day1.txt")
-    print(f"most calories: {max_cals}")
+    print(f"top 3 most calories: {max_cals} (total {sum(max_cals)})")
