ee0a848250f90f092c536c700a4637374b24fbaa
Lauren Gregg <lcgregg4@gmail.com>
2022-12-04 15:16:35 -0500

    reduced code; returns most calories

--- day1_caloriecounter.py
+++ day1_caloriecounter.py
@@ -1,23 +1,21 @@
+import heapq
+
 def calorie_counter(inventory):
     '''
-    return array of [elf_index, total_calories_being_carried]
+    return max_cals being carried
         inventory is a txt file where the elves write the calories of the items
-        a given elf's items are separated by a blank line
-        elf_index starts at 0
-        if 2 elves have the same # calories, the first elf index will be returned
+        a given elf's items are separated by a blank line\
     '''
     calorie_count = 0
-    total_cals = []
+    max_cals = 0
 
     f = open(inventory, "r")
     for x in f:
         if x!='\n':
             calorie_count += float(x)
         else:
-            total_cals.append(calorie_count)
+            max_cals = max(max_cals, calorie_count)
             calorie_count = 0
-
-    total_cals.append(calorie_count)
     f.close()
 
-    return [total_cals.index(max(total_cals)), max(total_cals)]
+    return max_cals
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from day1_caloriecounter import calorie_counter
 
 if __name__ == "__main__":
-    elf_w_most_cals = calorie_counter("input/day1.txt")
-    print(f"elf #{elf_w_most_cals[0]} has the most food ({elf_w_most_cals[1]}C)")
+    max_cals = calorie_counter("input/day1.txt")
+    print(f"most calories: {max_cals}")
